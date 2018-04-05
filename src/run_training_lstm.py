59a31373f9b69e72c043cfb36ff57dfef7199891
Chuan Yu <yuchuan0327@gmail.com>
2018-04-05 14:18:22 +0800

    Added evaluation for simple LSTM

--- src/run_training_lstm.py
+++ src/run_training_lstm.py
@@ -1,11 +1,12 @@
 from models.lstm import LSTM
-from train.train_lstm import train
+from train.train_lstm import train, evaluate
 import reader
+import torch
 from torch import optim
 import numpy as np
 from matplotlib import pyplot as plt
 
-use_cuda = True
+use_cuda = False
 
 input_size = 3
 output_size = 1
@@ -23,6 +24,9 @@ x = np.expand_dims(data_scaled[:-1], 1)
 y = data_scaled[1:, 0]
 y = y.reshape(y.shape[0], 1, 1)
 
+x = torch.from_numpy(x).contiguous()
+y = torch.from_numpy(y).contiguous()
+
 num_train = round(x.shape[0] * 0.6)
 num_val = round(x.shape[0] * 0.2)
 num_test = num_val
@@ -33,16 +37,22 @@ y_train = y[0:num_train]
 n_sequences_train = x_train.shape[0] // input_seq_len
 x_train = x_train[0:n_sequences_train * input_seq_len]
 y_train = y_train[0:n_sequences_train * input_seq_len]
-x_train = x_train.reshape([n_sequences_train, input_seq_len, 1, input_size])
-y_train = y_train.reshape([n_sequences_train, input_seq_len, 1, output_size])
+x_train = x_train.view([n_sequences_train, input_seq_len, 1, input_size])
+y_train = y_train.view([n_sequences_train, input_seq_len, 1, output_size])
 
 
 x_val = x[num_train:num_train + num_val]
 y_val = y[num_train:num_train + num_val]
+n_sequences_val = x_val.shape[0] // input_seq_len
+x_val = x_val[0:n_sequences_val * input_seq_len]
+y_val = y_val[0:n_sequences_val * input_seq_len]
+x_val = x_val.view([n_sequences_val, input_seq_len, 1, input_size])
+y_val = y_val.view([n_sequences_val, input_seq_len, 1, output_size])
+
+
 
 x_test = x[num_train + num_val:]
 y_test = y[num_train + num_val:]
-
 
 
 lstm = LSTM(input_size, hidden_size, output_size, n_layers)
@@ -61,5 +71,9 @@ for epoch in range(n_epochs):
         target_batches = y_train[i]
         train_loss = train(input_batches, target_batches, lstm, optimizer, use_cuda)
 
+
+    epoch_train_loss = evaluate(x_train, y_train, lstm)
+    epoch_val_loss = evaluate(x_val, y_val, lstm)
+
     print("epoch %i/%i" % (epoch + 1, n_epochs))
-    print("traing loss is %f" % train_loss)
+    print("traing loss is %f, validation loss is %f" % (epoch_train_loss, epoch_val_loss))
--- src/train/train_lstm.py
+++ src/train/train_lstm.py
@@ -5,18 +5,13 @@ import torch.nn.functional as F
 import numpy as np
 
 def train(input_batches, target_batches, lstm, optimizer, use_cuda=False):
-    input_batches = torch.from_numpy(input_batches)
-    target_batches = torch.from_numpy(target_batches)
+    # input_batches = torch.from_numpy(input_batches)
+    # target_batches = torch.from_numpy(target_batches)
 
     optimizer.zero_grad()
 
-    input_length = input_batches.shape[0]
-    batch_size = input_batches.shape[1]
-
     input_batches = Variable(input_batches, requires_grad=False)
     target_batches = Variable(target_batches, requires_grad=False)
-
-
 
     if use_cuda:
         input_batches = input_batches.cuda()
@@ -33,6 +28,57 @@ def train(input_batches, target_batches, lstm, optimizer, use_cuda=False):
     optimizer.step()
 
     return loss.data[0]
+
+def evaluate(inputs, targets, lstm, use_cuda=False):
+    n_batches = inputs.shape[0]
+    criterion = nn.MSELoss()
+    total_loss = 0
+    for i in range(n_batches):
+        input_batch = inputs[i]
+        target_batch = targets[i]
+        prediction_batch = predict(input_batch, lstm, use_cuda)
+        loss = get_mse(prediction_batch, target_batch)
+        total_loss += loss
+
+    loss_mean = total_loss / n_batches
+
+    return loss_mean
+
+def get_mse(predictions, targets):
+    predictions = predictions.view(predictions.numel())
+    targets = targets.view(targets.numel())
+    mse = ((predictions - targets) ** 2).mean()
+    return mse
+
+
+def predict(input_batches, lstm, use_cuda=False):
+
+    seq_len = input_batches.shape[0]
+    batch_size = input_batches.shape[1]
+
+    input_batches = Variable(input_batches, requires_grad=False)
+    all_predicitons = Variable(torch.zeros(seq_len, batch_size, 1))
+    if use_cuda:
+        input_batches = input_batches.cuda()
+        all_predicitons = all_predicitons.cuda()
+
+    x = input_batches[[0]]
+    for i in range(seq_len):
+        predictions = lstm(x)
+        all_predicitons[i] = predictions
+        if i < seq_len - 1:
+            features_next = input_batches[i+1, :, 1:].unsqueeze(0)
+            x = torch.cat((predictions, features_next), dim=2)
+
+    return all_predicitons.data
+
+
+
+
+
+
+
+
 
 
 
